dcffe1a552f7182d35169266f542d3e3c2081cbf
RedJocker <mauricio.guaruja@gmail.com>
2023-09-13 15:57:10 -0300

    from hyperskill

---
+++ Phrases/stage1/src/test/java/org/hyperskill/phrases/Stage1UnitTest.kt
@@ -0,0 +1,32 @@
+package org.hyperskill.phrases
+
+import org.hyperskill.phrases.internals.PhrasesUnitTest
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+// version 1.1
+@RunWith(RobolectricTestRunner::class)
+class Stage1UnitTest : PhrasesUnitTest<MainActivity>(MainActivity::class.java){
+
+    @Test
+    fun checkReminderTextView() {
+        testActivity {
+            reminderTv
+        }
+    }
+
+    @Test
+    fun checkRecyclerView() {
+        testActivity {
+            recyclerView
+        }
+    }
+
+    @Test
+    fun checkFloatingButton() {
+        testActivity {
+            floatingButton
+        }
+    }
+}
